329a72c03fc96fb2aa2fe4f27d004c90b53f6f44
puttiz <puttiz@gmail.com>
2011-05-16 21:22:33 +0800

    simple SCSS

--- config.rb
+++ config.rb
@@ -5,5 +5,7 @@ css_dir = "assets/c"
 sass_dir = "_scss"
 images_dir = "assets/i"
 javascripts_dir = "assets/j"
+
+# environment = "development"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
